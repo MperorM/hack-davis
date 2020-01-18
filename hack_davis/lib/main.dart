7796041bf0b45c5eef6361d3512902bc349b22d3
Mathias Kirk Bonde <mathiaskirkbonde@Mathiass-MacBook-Pro.local>
2020-01-18 15:50:22 -0800

    camera test

--- hack_davis/lib/main.dart
+++ hack_davis/lib/main.dart
@@ -1,30 +1,22 @@
-import 'package:flutter/material.dart';
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'dart:async';
 import 'homepage.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
-void main() => runApp(MyApp());
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    final File imageFile = getImageFile();
-
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.lime,
-      ),
-      home: MyHomePage(title: 'HackDavis Demo'),
-    );
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    logError(e.code, e.description);
   }
+  runApp(CameraApp());
 }
